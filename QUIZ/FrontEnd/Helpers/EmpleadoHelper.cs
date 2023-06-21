d9416963dfdf940fe868c8d73ad7ca6dfd643475
HansAbarca <68607913+HansAbarca@users.noreply.github.com>
2023-06-20 21:17:40 -0600

    Creador de vista

---
+++ QUIZ/FrontEnd/Helpers/EmpleadoHelper.cs
@@ -0,0 +1,29 @@
+﻿using FrontEnd.Models;
+using FrontEND.Helpers;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class EmpleadoHelper
+    {
+        ServiceRepository repository;
+
+        public EmpleadoHelper()
+        {
+            repository = new ServiceRepository();
+        }
+        public List<EmpleadoViewModel> GetAll()
+        {
+            List<EmpleadoViewModel> lista;
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Empleado");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<EmpleadoViewModel>>(content);
+            }
+
+
+            return new List<EmpleadoViewModel>();
+        }
+    }
+}
