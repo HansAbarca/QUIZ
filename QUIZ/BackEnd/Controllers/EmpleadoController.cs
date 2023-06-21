7191f13b919a936a5954ac9a401056f5b8792bb1
HansAbarca <68607913+HansAbarca@users.noreply.github.com>
2023-06-20 19:57:23 -0600

    empleado controller

---
+++ QUIZ/BackEnd/Controllers/EmpleadoController.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private IEmpleadoDAL empleadoDAL;
+
+        
+        public EmpleadoController()
+        {
+
+        }
+
+        // GET: api/<EmpleadoController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EmpleadoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmpleadoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EmpleadoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmpleadoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
